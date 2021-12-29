5cd37d1fe8a32d051d86bd3131ef9f09c06d3b5d
Tim Bell <tkbell51@gmail.com>
2021-12-29 09:13:34 -0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="page">
+        <div class="overlay"></div>
+        <Header />
+
+        <Nuxt class="content" />
+        <Footer />
+    </div>
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <Tutorial />
+</template>
+
+<script>
+export default {
+    head() {
+        return {
+            script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+        }
+    },
+}
+</script>
