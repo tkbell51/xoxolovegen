2fccd9a304bc88038b3e158303f172970163cedd
Tim Bell <tkbell51@gmail.com>
2022-01-01 13:22:38 -0500

    add getroutes to generate

--- nuxt.config.js
+++ nuxt.config.js
@@ -74,11 +74,8 @@ export default {
         },
     },
     generate: {
-        async routes() {
-            const { $content } = require('@nuxt/content')
-            const files = await $content({ deep: true }).only(['path']).fetch()
-
-            return files.map((file) => (file.path === '/index' ? '/' : file.path))
+        routes() {
+            return getRoutes()
         },
     },
     // Build Configuration: https://go.nuxtjs.dev/config-build
